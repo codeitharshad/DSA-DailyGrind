ce004ae63b334e3f83056325eab1cf8a71f52179
Harshad Gangurde <143880663+codeitharshad@users.noreply.github.com>
2024-06-26 14:19:20 +0530

    Added Four.java with implemetation of reverse an array

--- Four.java
+++ Four.java
@@ -42,7 +42,7 @@ public class Four {
          for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
-        System.out.println(); // For a new line after the array is printed
+        System.out.println();  // For a new line after the array is printed
     }
 }
 
